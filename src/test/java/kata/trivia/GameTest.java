8a5bd7a7c69c72aaee9b93569b21e0d37f4af0a2
Hidetada-komatu <2543144515@qq.com>
2017-12-31 22:58:45 +0800

    Add user intent test the_game_should_be_over_if_a_player_rolls_the_dice_for_8_times_and_answers_the_question_wrongly_for_1_tme_followed_by_an_even_rolling_number_but_then_correctly_for_7_times

--- src/test/java/kata/trivia/GameTest.java
+++ src/test/java/kata/trivia/GameTest.java
@@ -35,9 +35,9 @@ public class GameTest {
 //Act
         game.roll(1);
         game.wrongAnswer();
-        game.roll(2);
+        game.roll(1);
         game.wasCorrectlyAnswered();
-        for(int i = 0; i < 6; i++){
+        for(int i = 0; i < 5; i++){
             game.roll(1);
             isGameStillInProgress = game.wasCorrectlyAnswered();
         }
@@ -46,7 +46,24 @@ public class GameTest {
 
     }
     //    TODO-user-intent: the game should be over if a player rolls the dice for 8 times and answers the question wrongly for 1 tme followed by an even rolling number but then correctly for 7 times.
-
+    @Test
+    public void the_game_should_be_over_if_a_player_rolls_the_dice_for_8_times_and_answers_the_question_wrongly_for_1_tme_followed_by_an_even_rolling_number_but_then_correctly_for_7_times() {
+//Arrange
+        Game game = new Game();
+        game.add("Chet");
+        boolean isGameStillInProgress = true;
+//Act
+        game.roll(1);
+        game.wrongAnswer();
+        game.roll(2);
+        game.wasCorrectlyAnswered();
+        for (int i = 0; i < 6; i++) {
+            game.roll(1);
+            isGameStillInProgress = game.wasCorrectlyAnswered();
+        }
+//Assert
+        assertFalse(isGameStillInProgress);
+    }
     /**
      * Rigourous Test :-)
      */
