b4ed7ea0d136a31e162a63dca288a21ac28142bf
Hidetada-komatu <2543144515@qq.com>
2017-12-31 22:19:32 +0800

    Add 3 user intent tests as TODOs

--- src/test/java/kata/trivia/GameTest.java
+++ src/test/java/kata/trivia/GameTest.java
@@ -8,6 +8,11 @@ import java.util.Random;
 import static org.junit.Assert.assertEquals;
 
 public class GameTest {
+//    TODO-user-intent: the game should be over if a player rolls the dice and answers each question correctly for 6 times
+
+//    TODO-user-intent: the game should be over if a player rolls the dice for 7 times and answers the question wrongly for 1 time followed by an odd rolling number but then correctly for 6 times.
+
+//    TODO-user-intent: the game should be over if a player rolls the dice for 8 times and answers the question wrongly for 1 tme followed by an even rolling number but then correctly for 7 times.
     /**
      * Rigourous Test :-)
      */
